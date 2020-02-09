6b6f74d578397ca6c833e735c5aa22d283d7a6ba
filadelfia46 <45551633+filadelfia46@users.noreply.github.com>
2020-02-09 12:49:42 +0300

    2 poiya zavershino

--- blood.py
+++ blood.py
@@ -13,7 +13,6 @@ root1.configure()
 root1.title("Касания")
 
 
-
 #построение окружности
 def chengtext1():
     #cl = ['#5d44a2','#b5319b','#ff9308','#960018','#082567','#990066','#6e7eae']
@@ -189,8 +188,6 @@ def osnova():
     but = Button(root3, text='Начертить',width=20, font=("Arial Bold", 10), command = chengtext1,fg='#535a53',bg='#a5e9c1')
     but.pack(side=TOP)
 
-
-
     #промежуток 3
     size = 600
     canvas2 = Canvas(root, width=60, height=40)
@@ -212,32 +209,94 @@ def nastoiki():
     b7.destroy()
 
     #поле настроек
-    b70 = Canvas(root1, width=500, height=600)
-    b70.pack()
+    b73 = Canvas(root1, width=500, height=600,bg='#d6e4ba')
+    b73.pack()
 
     #настройки текст
-    b70 = Label(root1, text="Выберите размер поля", font=("Arial Bold", 15),fg='#535a53')
-    b70.place(x=143,y=10)
+    b70 = Label(root1, text="Выберите размер поля", font=("@Microsoft YaHei UI Light", 15),fg='#0a0000',bg='#d6e4ba')
+    b70.place(x=143,y=14)
 
     #кнопка далее
-    b71 = Button(root1, text='Далее',width=20, font=("Arial Bold", 10), command = osnova,fg='#535a53',bg='#a5e9c1')
+    b71 = Button(root1, text='Далее',width=20, font=("@Microsoft YaHei UI Light", 10), command = osnova,fg='#0a0000',bg='#b0d3bf')
     b71.place(x=160,y=490)
     
+    #первое поле ввода
+    c1 = Entry(root1, width=10,bg='#dee5de')
+    c1.place(x=150,y=270)
 
+    #круги над полями ввода
+    s1 = b73.create_oval(170,240,190,260,outline="#e17576", fill="#e17576")
+    s1 = b73.create_oval(320,240,340,260,outline="#3a75c4", fill="#3a75c4")
+    
+    #текст на
+    b = Label(root1, text="на", font=("@Microsoft YaHei UI Light", 15),fg='#0a0000',bg='#d6e4ba')
+    b.place(x=240,y=260)
+    
+    #второе поле ввода
+    c2 = Entry(root1, width=10,bg='#dee5de')
+    c2.place(x=297,y=270)
+
+    #слова о размерности
+    b = Label(root1, text="*в пикселях", font=("@Microsoft YaHei UI Light", 9),fg='#6c6f74',bg='#d6e4ba')
+    b.place(x=220,y=45)
+
+    #рисунок системы координат
+    b73.create_line(50,100,450,100,width=7,fill='#e17576')
+    b73.create_line(50,97,50,480,width=7,fill='#3a75c4')
+    
+    b73.create_line(450,100,450-15,100-15,width=5,fill="#e17576")
+    b73.create_line(450,100,450-15,100+15,width=5,fill="#e17576")
+    b73.create_line(50,480,50-15,480-15,width=5,fill="#3a75c4")
+    b73.create_line(50,480,50+15,480-15,width=5,fill="#3a75c4")
+    
+    #s1 = b73.create_oval(40,110,60,90,outline="#6c4f7e", fill="#6c4f7e")
+    
+    b73.create_line(150,95,150,107,width=3,fill="#e17576") 
+    b73.create_line(250,95,250,107,width=3,fill="#e17576")
+    b73.create_line(350,95,350,107,width=3,fill="#e17576")
+    
+    b73.create_line(42,200,57,200,width=3,fill="#3a75c4")
+    b73.create_line(42,300,57,300,width=3,fill="#3a75c4")
+    b73.create_line(42,400,57,400,width=3,fill="#3a75c4")
+
+    b = Label(root1, text="100", font=("@Microsoft YaHei UI Light", 9),fg='#6c6f74',bg='#d6e4ba')
+    b.place(x=135,y=110)
+
+    b = Label(root1, text="200", font=("@Microsoft YaHei UI Light", 9),fg='#6c6f74',bg='#d6e4ba')
+    b.place(x=235,y=110)
+
+    b = Label(root1, text="300", font=("@Microsoft YaHei UI Light", 9),fg='#6c6f74',bg='#d6e4ba')
+    b.place(x=335,y=110)
+    
+
+    b = Label(root1, text="100", font=("@Microsoft YaHei UI Light", 9),fg='#6c6f74',bg='#d6e4ba')
+    b.place(x=60,y=195)
+
+    b = Label(root1, text="200", font=("@Microsoft YaHei UI Light", 9),fg='#6c6f74',bg='#d6e4ba')
+    b.place(x=60,y=295)
+
+    b = Label(root1, text="300", font=("@Microsoft YaHei UI Light", 9),fg='#6c6f74',bg='#d6e4ba')
+    b.place(x=60,y=395)
+    
     
 #поле старта
 canvas20 = Canvas(root1, width=500, height=600,bg='#d6e4ba')
 canvas20.pack()
 
 #кнопка далее
-but11 = Button(root1, text='Далее',width=20, font=("@Microsoft YaHei UI Light", 10), command = nastoiki,fg='#5a9696',bg='#bbdb93')
+but11 = Button(root1, text='Далее',width=20, font=("@Microsoft YaHei UI Light", 10), command = nastoiki,fg='#0a0000',bg='#b0d3bf')
 but11.place(x=160,y=490)
 
 #слова Касании
-b6 = Label(root1, text="Я - Касания", font=("@Microsoft YaHei UI Light", 15),fg='#0b272d',bg='#d6e4ba')
+b6 = Label(root1, text="Я Касания", font=("@Microsoft YaHei UI Light", 15),fg='#0a0000',bg='#d6e4ba')
 b6.place(x=200,y=10)
-b7 = Label(root1, text="программа для создания чертежей дорожек", font=("@Microsoft YaHei UI Light", 10),fg='#0b272d',bg = '#d6e4ba')
+b7 = Label(root1, text="программа для создания чертежей дорожек", font=("@Microsoft YaHei UI Light", 10),fg='#0a0000',bg = '#d6e4ba')
 b7.place(x=123,y=35)
 
+
+#рисунок
+s1 = canvas20.create_oval(150,180,350,380,outline="#e8ba73", fill="#e8ba73")
+s2 = canvas20.create_oval(150,150,250,250,outline="#c37028", fill="#c37028")
+s3 = canvas20.create_oval(400,420,350,370,outline="#4b7674", fill="#4b7674")
 
 root1.mainloop()
